66a6575af45e37f5ad60b8e18d51097de9ef374e
Ak8388 <akbarraw09@gmail.com>
2024-06-20 21:47:46 +0700

    add[comment]

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ package tugas_1;
 
 import java.util.*;
 
+// Annur Akbar Bahagia
+// BE(Java) SKSK Batch 23
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
